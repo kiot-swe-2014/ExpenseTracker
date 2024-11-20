885e957fc1762b0ffaf91b3a98b6c65c4f31a8c3
abdurehmanSualih <abdurehmansualih00@gmail.com>
2024-11-20 22:50:39 +0300

    finish msql connection

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -4,5 +4,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/expense_tracker_db";
+    private static final String USER = "root"; // Change to your MySQL username
+    private static final String PASSWORD = "1234"; // Change to your MySQL password
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to MySQL.");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to MySQL: " + e.getMessage());
+        }
+        return conn;
+    }
 }
