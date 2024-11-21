37caeda1aefa52b992aee43aa6365f3283910e96
abdurehmanSualih <abdurehmansualih00@gmail.com>
2024-11-21 22:38:54 +0300

    impliment registerUser method

--- src/UserDAO.java
+++ src/UserDAO.java
@@ -8,6 +8,26 @@ public class UserDAO {
     private static final String USER = "root";
     private static final String PASS = "1234"; 
 
-   
+    // Method for user registration
+    public boolean registerUser(String username, String password, String email) {
+        String query = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setString(1, username);
+            statement.setString(2, password);
+            statement.setString(3, email);
+
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    
 }
 
