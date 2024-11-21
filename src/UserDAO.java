2565663a01218509f9cab552aab3471d6990ce69
abdurehmanSualih <abdurehmansualih00@gmail.com>
2024-11-21 22:36:58 +0300

    setup database

--- src/UserDAO.java
+++ src/UserDAO.java
@@ -4,6 +4,10 @@ import java.sql.*;
 
 public class UserDAO {
 
-    
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/expense_tracker_db";
+    private static final String USER = "root";
+    private static final String PASS = "1234"; 
+
+   
 }
 
